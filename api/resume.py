3577b4d1ca630b9380dfb3b7eb682e4bb21fc837
Ayse <aysesadioglu@protonmail.com>
2024-03-14 23:33:59 +0300

    comment

--- api/resume.py
+++ api/resume.py
@@ -7,7 +7,7 @@ resume_route = Blueprint('resume_route', __name__)
 @resume_route.route("/api/resume/upload", methods=['POST'])
 def upload():
     if 'file' not in request.files:
-        return 'No file part'
+        return 'No file part' #TODO:404
 
     file = request.files['file']
     return upload_service(file)
